6b14e8d13b0fb31986c4a729bf2294da143c5125
Miriam <davydov-miriam@hotmail.com>
2024-04-07 15:29:48 -0400

    Correction Canada

--- src/drapeaux/canada.py
+++ src/drapeaux/canada.py
@@ -10,11 +10,9 @@ class Canada(DrapeauAvecRectangles):
     def __init__(self, nom, couleurs):
         super().__init__(nom, couleurs, nb_rectangles=2)
         self.nb_erables = 1
-        self.marge_erreur = 1
 
     def _image_contient_erables(self, image):
-        gris = image.convertir_niveaux_de_gris()
-        gris = cv2.GaussianBlur(gris, (5, 5), 0)
+        gris = image.convertir_niveaux_de_gris_ameliore()
 
         seuil_adaptatif = cv2.adaptiveThreshold(gris, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
         contours, _ = cv2.findContours(seuil_adaptatif, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -26,7 +24,7 @@ class Canada(DrapeauAvecRectangles):
                 erables.append(approx)
                 image.dessiner_contours("Contours feuilles erables", [approx])
 
-        return verifier_avec_marge_erreur(len(erables), self.nb_erables, self.marge_erreur)
+        return len(erables) == self.nb_erables
 
     def valider(self, image: Image):
         return (self.couleurs_valides(image.couleurs)
