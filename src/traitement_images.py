a580f177ebcf6747d37275aa2c667b58110f7969
Miriam <davydov-miriam@hotmail.com>
2024-04-15 12:49:01 -0400

    Ménage

--- src/traitement_images.py
+++ src/traitement_images.py
@@ -73,31 +73,5 @@ class TraitementImages:
 
             while True:
                 if cv2.waitKeyEx(1) == 27:  # Faire escape pour quitter ou pour passer à l'image suivante
+                    cv2.destroyAllWindows()
                     break
-
-    # def afficher_histogramme_tout(self):
-        # for image in self.images:
-            # self._afficher('Image originale', image)
-
-            # bleu, vert, rouge = cv2.split(image)  # Division en canaux de couleur
-
-            # hist_bleu = cv2.calcHist([bleu], [0], None, [256], [0, 256])
-            # hist_vert = cv2.calcHist([vert], [0], None, [256], [0, 256])
-            # hist_rouge = cv2.calcHist([rouge], [0], None, [256], [0, 256])
-
-            # plt.plot(hist_bleu, color='blue', label='Bleu')
-            # plt.plot(hist_vert, color='green', label='Vert')
-            # plt.plot(hist_rouge, color='red', label='Rouge')
-
-            # plt.title('Histogramme couleur de l\'image')
-            # plt.xlabel('Intensité des pixels')
-            # plt.ylabel('Nombre de pixels')
-            # plt.legend()
-
-            # plt.savefig('histogramme.png')
-            # plt.close()
-
-            # hist_image = cv2.imread('histogramme.png')
-            # cv2.imshow('Histogramme', hist_image)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
